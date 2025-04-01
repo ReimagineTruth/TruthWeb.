fa231a6b2712f616443365dece0488e1452032d8
Mrwain <reimaginetruth@gmail.com>
2025-04-01 23:11:26 +0800

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,1 +1,1 @@
-apikey = "2xyhqix2bidbmijpgkhjyiusbkhzhvofyifac3asn8nc6dgee677wr6glndxkiw1"
+apikey = "fukqtl4kyz2ijkjilcx1bfhfnx4f8qcvmllaqajrncetgyvo4z8qotuqcnyorx9x"  # Replace with your actual Pi Network API key
