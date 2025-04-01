5cbbc6f1417d543831f835da6ed3e1565ee70734
Mrwain <reimaginetruth@gmail.com>
2025-04-01 23:10:06 +0800

    Update variables.py

--- truthweb/variables.py
+++ truthweb/variables.py
@@ -1,1 +1,1 @@
-apikey = "YOUR_PI_API_KEY"  # Replace with your actual Pi Network API key
+apikey = "fukqtl4kyz2ijkjilcx1bfhfnx4f8qcvmllaqajrncetgyvo4z8qotuqcnyorx9x"  # Replace with your actual Pi Network API key
