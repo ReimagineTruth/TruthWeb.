0647cd63dcdf1e1b9cc6b4afb40ad7c01411da2e
Mrwain <reimaginetruth@gmail.com>
2025-04-02 19:30:49 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
-// Your routes, middleware, etc. go here
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Start the server
+// Define a route for the root path '/'
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start the server on port 5000
 app.listen(5000, () => {
   console.log('Server running on port 5000');
 });
